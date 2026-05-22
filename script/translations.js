9f9ce862d8974fdf65287afc73e1ffed5bffec84
pe2-73134823 <235252853+pe2-73134823@users.noreply.github.com>
2026-05-22 23:13:03 +0200

    add translations to home page

---
+++ script/translations.js
@@ -0,0 +1,33 @@
+if (localStorage.getItem("lang") == "swe") {
+    langInit("swe");
+}
+function changeLanguage() {
+    if (localStorage.getItem("lang") == "swe") {
+        localStorage.removeItem("lang");
+        location.reload();
+    } else {
+        localStorage.setItem("lang", "swe")
+        langInit("swe");
+    }   
+}
+async function langInit(langpicked) {
+    const langfiletofetch = "assets/translations/" + langpicked + ".txt"
+    let langarray = await loadTxtFile(langfiletofetch);
+    let langidarray = await loadTxtFile("assets/translations/ids.txt");
+    for (let i = 0; i < langarray.length; i++) {
+        document.getElementById(langidarray[i]).innerHTML = langarray[i];
+    }
+}
+async function loadTxtFile(filetofetch) {
+  try {
+    const fileresponse = await fetch(filetofetch);
+    if (!fileresponse.ok) {
+      throw new Error(`err: ${fileresponse.status}`);
+    }
+    const textFileContent = await fileresponse.text();
+    const textFileArray = textFileContent.split(/\r\n|\n/)
+    return textFileArray;
+  } catch (error) {
+    console.error('file loading err:', error);
+  }
+}
